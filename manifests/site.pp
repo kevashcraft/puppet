03417369a44f290729d685c559e3a3cf1281c47e
Kevin Ashcraft <kevin@kevashcraft.com>
2017-11-06 17:24:48 -0500

    hiera global config

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,4 +6,12 @@ node 'k4k.ha.kevashcraft.com' {
   include linux
   include dhcpd
   include named
+  include firewalld
+}
+
+node 'maint.kevashcraft.com' {
+  include linux
+  include named
+  include firewalld
+  include sshd
 }
