f5a847be3013dd04f892c3a5611e8f7acf86a4ae
Kevin Ashcraft <kevin@kevashcraft.com>
2017-11-06 17:23:42 -0500

    linux global config

--- modules/linux/manifests/init.pp
+++ modules/linux/manifests/init.pp
@@ -6,28 +6,30 @@
 # Parameters
 # ----------
 #
-# * `$domain_name`
-#   The name of the domain, defined in domain::name and passed to linux::domain_name,
-#   is used in resolv.conf.
+# * `$search_domains`
+#   The list of domains to search resolv.conf.
+#
+# * `$nodes`
+#   The list of global nodes, refined to a list of domain names for resolv.conf.
 #
 # * `$nameserver`
-#   The nameserver, defined in domain::&id_dns_server_ip and passed to linux::nameserver,
+#   The nameserver, defined in domain::&dns_ip and passed to linux::nameserver,
 #   is used in resolv.conf.
 #
 # * `$users`
 #   The node-specifc list of users, defined in linux::users,
 #   is used to create the users.
 #
-# * `$users_global`
-#   The global list of users, defined domain::users and passed to linux::users_global,
-#   is used to create the users.
-#
 # * `$groups`
 #   The node-specifc list of groups, defined in linux::groups,
 #   is used to create the groups.
 #
-# * `$groups_global`
-#   The global list of groups, defined domain::groups and passed to linux::groups_global,
+# * `$global_users`
+#   The global list of users, defined domain::users and passed to linux::global_users,
+#   is used to create the users.
+#
+# * `$global_groups`
+#   The global list of groups, defined domain::groups and passed to linux::global_groups,
 #   is used to create the groups.
 #
 # Examples
@@ -49,12 +51,18 @@
 # Copyright 2017 Kevin Ashcraft MIT License
 #
 class linux (
-  $domain_name,
+  $nodes,
+  $search_domains,
   $nameserver,
+  $root_pw,
   $users,
-  $users_global,
   $groups,
-  $groups_global,
+  $global_users,
+  $global_groups,
+  $packages,
+  $services,
+  $global_packages,
+  $global_services,
 ) {
 
   file { '/etc/resolvconf.conf':
@@ -65,32 +73,82 @@ class linux (
     content => template('linux/etc/resolv.conf.erb'),
   }
 
-  ($groups_global + $groups).each |$group| {
+  file { '/etc/hostname':
+    content => template('linux/etc/hostname.erb'),
+  }
+
+  ($global_groups + $groups).each |$group| {
     group { $group:
       ensure   => present,
     }
   }
 
-  ($users_global + $users).each |$user| {
+  user { 'root':
+    ensure   => present,
+    password => $root_pw,
+  }
+
+  ($global_users + $users).each |$user| {
     user { $user['username'] :
-      ensure   => present,
-      comment  => $user['comment'],
-      home     => "/home/${user['username']}",
-      password => $user['password'],
-      shell    => '/bin/bash',
+      ensure     => present,
+      comment    => $user['comment'],
+      managehome => true,
+      home       => "/home/${user['username']}",
+      groups     => $user['groups'],
+      password   => $user['password'],
+      shell      => '/bin/bash',
+    }
+
+    file { "/home/${user['username']}":
+      ensure  => directory,
+      require => User[$user['username']],
+      owner   => 'kevin',
+      group   => 'kevin',
     }
 
     file { "/home/${user['username']}/.ssh":
       ensure  => directory,
-      require => User[$user['username']],
+      require => File["/home/${user['username']}"],
+      owner   => 'kevin',
+      group   => 'kevin',
     }
 
     file { "/home/${user['username']}/.ssh/authorized_keys":
-      ensure => directory,
-      source => [
+      require => File["/home/${user['username']}/.ssh"],
+      owner   => 'kevin',
+      group   => 'kevin',
+      source  => [
         "puppet:///modules/linux/home/${user['username']}/.ssh/authorized_keys",
         'puppet:///modules/linux/home/default/.ssh/authorized_keys',
       ],
+    }
+  }
+
+  ($global_packages + $packages).each |$package| {
+    if ($package == 'vim') {
+      case $::osfamily {
+        'redhat': {
+          $package_actual = 'vim-enhanced'
+        }
+        default: {
+          $package_actual = 'vim'
+        }
+      }
+    } else {
+      $package_actual = $package
+    }
+
+    package { $package_actual:
+      ensure   => present,
+    }
+  }
+
+  ($global_services + $services).each |$service| {
+    service { $service:
+      ensure => running,
+      enable => true,
+      #hasrestart => true,
+      #hasstatus  => true,
     }
   }
 }
